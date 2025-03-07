d101667ccfb58eac0dbdbd982d1046b9a2be58a3
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2025-03-07 21:18:33 +0900

    add ZodNumber\

--- vendor/src/lib.rs
+++ vendor/src/lib.rs
@@ -1,13 +1,45 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+mod zod;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    // The `console.log` is quite polymorphic, so we can bind it with multiple
+    // signatures. Note that we need to use `js_name` to ensure we always call
+    // `log` in JS.
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
+    // Multiple arguments too!
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, vendor!");
+    console_log!("Hello {}!", "world");
+    console_log!("Let's print some numbers...");
+    console_log!("1 + 3 = {}", 1 + 3);
+}
+
+#[wasm_bindgen]
+pub fn is_number(value: JsValue) -> bool {
+    zod::ZodNumber::new()._parse(&value).status == "ok"
+
+    // if value.as_f64().is_some() {
+    //     return true;
+    // }
+    // false
 }
